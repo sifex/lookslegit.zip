5a5198c307e748ad10b4c72bb95576ace1c1fbe9
Alex <github@sinn.io>
2023-05-18 13:30:15 +0100

    wip

--- server.js
+++ server.js
@@ -12,22 +12,31 @@ fs.readFile('./index.html', function (err, html) {
 
     http.createServer((req, res) => {
         if (req.headers.authorization) {
-            const url = atob(req.headers.authorization.replace('Basic ', '')).replaceAll('â§¸', '/')
-            proxy(req, {
-                url: url,
-                onResponse(response) {
-                    response.headers.warning = 'This response was modified, and the results should not be trusted. Like at all.';
-                },
-                skipModifyResponse(response) {
-                    return !proxy.mime.isText(response.contentType.type);
-                },
-                modifyResponse(response) {
-                    // change headers
-                    response.headers['content-type'] = 'application/json; charset: utf8';
-                    // use new content, can be string, buffer, null, undefined or object;
-                    response.body = 'lol owned' + "\n\n" + response.body;
-                }
-            }, res);
+            try {
+                let url = decodeURI(atob(req.headers.authorization.replace('Basic ', '')).replaceAll('â§¸', '/'))
+
+                if (!url.startsWith('http')) { url = 'https://'+url }
+                if (url.endsWith(':')) { url = url.replace(/:+$/, '') }
+                console.log(url)
+                proxy(req, {
+                    url: url,
+                    onResponse(response) {
+                        response.headers.warning = 'This response was modified, and the results should not be trusted. Like at all.';
+                    },
+                    skipModifyResponse(response) {
+                        return !proxy.mime.isText(response.contentType.type);
+                    },
+                    modifyResponse(response) {
+                        // change headers
+                        response.headers['content-type'] = 'application/json; charset: utf8';
+                        // use new content, can be string, buffer, null, undefined or object;
+                        response.body = 'lol owned' + "\n\n" + response.body;
+                    }
+                }, res);
+            } catch (e) {
+                res.writeHead(200);
+                res.end(`whoops\n`);
+            }
         } else {
             var done = finalhandler(req, res);
             serve(req, res, done);
